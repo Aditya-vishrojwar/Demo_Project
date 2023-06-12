a13cea87829a90f68d82d50ecdb4b8ddafe2f31a
Aditya-vishrojwar <134601452+Aditya-vishrojwar@users.noreply.github.com>
2023-06-12 19:45:31 +0530

    change to hello world

--- fisrt.java
+++ fisrt.java
@@ -1,6 +1,6 @@
 class hello{
     public static void main(String []args){
-        System.out.println("this is hello world");
+        System.out.println("this is hello world in java programing");
 
     }
 }
